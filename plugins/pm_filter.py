27c4b55f3694d3c0569e6d251d07dcaaf4d8b8a0
PaulWalker <91833744+Vampire421@users.noreply.github.com>
2021-10-24 15:23:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -144,22 +144,25 @@ async def group(client, message):
                     ]
                 )
         else:
-            Alpha=await client.send_message(
+            LuciferMoringstar=await client.send_message(
             chat_id = message.chat.id,
             text=f"""
-<b>👋Hey {message.from_user.mention}</b>
-
-<b>Sorry, No Movie/Series Related to the Given Word Was Found 🥺</b>
-
-<b>Please Go to Google and Confirm the Correct Spelling 🙏</b>
-
-<b>Click Here To 👉 <a href='https://www.google.com'>🔍 Search 🔎</a> </b>
-
-<b>✍Or Your Spelling Is Correct Report To Admins For Add Requested File :- @admins</b>""",
-            
-            parse_mode="html",
-            reply_to_message_id=message.message_id
-        )
+👋Hey {message.from_user.mention}
+If this movie is not in our database you will not get that movie.
+Otherwise, the spelling of the name of the requested movie may not be correct.
+So you go to google and check the spelling of the name of the movie you want.
+If you do not receive the movie even after entering the correct name.
+@admin type movie name Inform the admin in this format.
+We will upload within 24 hours as we can.
+ഈ സിനിമ ഞങ്ങളുടെ ഡാറ്റാബേസിൽ ഇല്ലെങ്കിൽ നിങ്ങൾക്ക് ഈ സിനിമ ലഭിക്കില്ല.
+അല്ലെങ്കിൽ, അഭ്യർത്ഥിച്ച സിനിമയുടെ പേരിന്റെ അക്ഷരവിന്യാസം ശരിയായിരിക്കില്ല.
+അതിനാൽ നിങ്ങൾ ഗൂഗിളിൽ പോയി നിങ്ങൾക്ക് ആവശ്യമുള്ള സിനിമയുടെ പേരിന്റെ സ്പെല്ലിംഗ് പരിശോധിക്കുക.
+ശരിയായ പേര് നൽകിയിട്ടും നിങ്ങൾക്ക് സിനിമ ലഭിക്കുന്നില്ലെങ്കിൽ.
+@admin ടൈപ്പ് മൂവി നാമം ഈ ഫോർമാറ്റിൽ അഡ്മിനെ അറിയിക്കുക.
+ഞങ്ങൾ 24 മണിക്കൂറിനുള്ളിൽ അപ്‌ലോഡ് ചെയ്യാൻ ശ്രമിക്കും""",
+              parse_mode="html",
+              reply_to_message_id=message.message_id
+           )
             return
         if not btn:
             return
